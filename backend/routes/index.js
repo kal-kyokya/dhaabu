9fb2771906adb478f6640992dd1bf2f79e59fcdf
kal-kyokya <kalkyokya4@gmail.com>
2025-01-03 02:43:40 -0800

    Resolve ESLint conflicts.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,21 +1,25 @@
-//Contains all our API endpoints
+// This file contain all the app's API endpoints
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-
+import UsersController from '../controllers/UsersController';
 
 export default function routing(app) {
+  // Create a router instance
   const router = new Router();
+
+  // Mount the router on '/' and all subsequent paths
   app.use('/', router);
+
   // AppController
   router.get('/status', AppController.getStatus);
   router.get('/stats', AppController.getStats);
+
   // UsersController
   router.post('/users', UsersController.postNew);
   router.get('/users/me', UsersController.getMe);
+
   // AuthController
   router.get('/connect', AuthController.getConnect);
   router.get('/disconnect', AuthController.getDisconnect);
 }
-
