b962cbc3be10a912db65710a2ec2baf0cd585b97
Nazavivian21 <nazafavourukwe18@gmail.com>
2025-01-06 12:18:27 +0000

    frontend dev files

---
+++ frontend/src/api.js
@@ -0,0 +1,14 @@
+// src/pages/Login.jsx
+
+// src/pages/Signup.jsx
+
+// src/pages/Dashboard.jsx
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export const login = (data) => API.post('/login', data);
+export const signup = (data) => API.post('/signup', data);
+
