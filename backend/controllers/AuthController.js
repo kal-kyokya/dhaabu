7c4005ab0518e167e9b0139e526d3b920ab3dafd
kal-kyokya <kalkyokya4@gmail.com>
2025-01-07 03:40:48 -0800

    Investigate redis load error.

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,37 +1,46 @@
-//File containing endpoints that will authenticate the user
+// File containing endpoints authenticating a user
+import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
-import { v4 as uuidv4 } from 'uuid';
-import { ObjectId } from 'mongodb';
 
 export default class AuthController {
-  //Signs in the user by generating new auth token
+  /**
+   * Sign in a user by generating a new Auth Token
+   * @param { Object } req - The request object
+   * @param { Object } res - The response object
+   */
   static async signingIn(req, res) {
+    // Sign in a user using 'Basic Authentication'
     const header = req.headers.authorization;
-    if(!header) {
-      return res.status(401).send({ 'error': 'Unauthorized' });
+    if (!header) {
+      return res.status(401).send({ error: 'Unauthorized' });
     }
+
+    // Extract user information from the encoded header
     const credentials = Buffer.from(header.split(' ')[1], 'base64').toString('utf-8');
     const email = credentials.split(':')[0];
-    const password = credentials.split(':')[1];
 
+    // Ensure the DB contains a user with input email
     const user = await (await dbClient.client.db()).collection('users').findOne({ email });
-    if(!user) {
-      return res.status(401).send({ 'error': 'Unauthorized' });
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
     }
 
+    // Generate an Auth Token and associate it to the user
     const token = uuidv4();
     const key = `auth_${token}`;
     await redisClient.set(key, user._id, 86400);
-    return res.status(200).send({ "token": token });
+    return res.status(200).send({ token: token });
   }
-  
-  // Will sign out user based on token
+
+  /**
+   * Sign out a user based on an Auth Token
+   * @param { Object } req - The request object
+   * @param { Object } res - The response object
+   */
   static async signingOut(req, res) {
     const key = req.key;
-    console.log(key);
     await redisClient.del(key);
     return res.status(204).send({});
   }
-
 }
