265c620f3a91fb2363b6b53e99f11359e567ed52
Patrick Mutabazi <patrickmtbz@gmail.com>
2025-01-12 01:04:00 +0300

    Resolved user login functionality

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,13 +10,21 @@ const API = axios.create({
 });
 
 export const login = ({email, password}) => {
-  const auth = `Basic ${email}:${password}`;
-  const headers = new Headers();
-  headers.append("authorization", auth);
-  console.log({email, password});
+  const token = btoa(`${email}:${password}`);
+  const auth = `Basic ${token}`;
+  /**
+   * const headers = new Headers();  Is incorrect
+   * headers.append("Authorization", auth);  axios expects headers to be passed as
+   * plain Javascript object as follows
+   *
+   * const headers = { Authorization: auth};
+   * return API.get('/signIn', { headers });
+   *
+   * or use global axios config defaults as follows
+   */
+  API.defaults.headers.common['Authorization'] = auth;
 
-  return API.get('/signIn', {headers});
+  return API.get('/signIn');
 };
 
 export const signup = (data) => API.post('/users/signUp', data);
-
