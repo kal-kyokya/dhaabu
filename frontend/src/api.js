c02af8ec22721e4a7935429b981eff9c50cbaca7
kal-kyokya <kalkyokya4@gmail.com>
2025-02-11 11:23:21 +0000

    Redefine axios communicaion from React App to MongoDB backend.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,34 +1,7 @@
-// src/pages/Login.jsx
-
-// src/pages/Signup.jsx
-
-// src/pages/Dashboard.jsx
 import axios from 'axios';
 
-/**
- * const API = axios.create({
- * baseURL: import.meta.env.VITE_API_URL,
- * });
- */
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
-const baseURL = import.meta.env.VITE_API_URL;
+export const login = (data) => API.get(`signIn`, data);
 
-export const login = ({email, password}) => {
-  // const token = btoa(`${email}:${password}`);
-  // const auth = `Basic ${token}`;
-  /**
-   * const headers = new Headers();  Is incorrect
-   * headers.append("Authorization", auth);  axios expects headers to be passed as
-   * plain Javascript object as follows
-   *
-   * const headers = { Authorization: auth};
-   * return API.get('/signIn', { headers });
-   *
-   * or use global axios config defaults as follows
-   */
-  // API.defaults.headers.common['Authorization'] = auth;
-
-    return axios.get(`${baseURL}/signIn`, { email, password });
-};
-
-export const signup = (data) => axios.post(`${baseURL}/users/signUp`, data);
+export const signup = (data) => API.post(`users/signUp`, data);
