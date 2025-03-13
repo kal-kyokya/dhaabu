b6ac9fe8753f7d8c153a47242f6ddbcff68796cf
kal-kyokya <kalkyokya4@gmail.com>
2025-03-13 11:28:39 +0000

    Test the 'Sign up' process

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
-export const login = (data) => API.get(`signIn`, data);
+export const login = (data) => API.get('signIn', data);
 
-export const signup = (data) => API.post(`users/signUp`, data);
+export const signup = async (data) => API.post('users/signUp', data);
